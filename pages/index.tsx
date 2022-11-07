133a8c9e4b143db3875b0d3d76442678ed91638b
Simeon Lees <simeon.k.lees@gmail.com>
2022-11-07 09:18:58 +0000

    Index tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ export default function Index() {
           <Navigation/>
           <section>
             <h1 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">Hi, I'm Simeon Lees.</h1>
+            <p>Hey, I'm a Frontend Engineer with a passion for crafting <strong>delightful user experiences</strong>. I'm currently a Tech Lead at <a className="text-blue" href="https://www.hubspot.com">HubSpot</a> building out its collaboration platform.</p>
             </section>
 
         </Container>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         'accent-7': '#333',
         success: '#0070f3',
         cyan: '#79FFE1',
+        blue: '#74b0ff',
       },
       spacing: {
         28: '7rem',
