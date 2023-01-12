36149491a34d59d85d54d0a84900061592b1c4c5
Simeon Lees <simeon.k.lees@gmail.com>
2023-01-12 08:52:52 +0000

    Prism styling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href={`https://unpkg.com/prismjs@0.0.1/themes/prism-okaidia.css`}
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
