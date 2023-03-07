8740d2ab5c611bbccb7a7acc7097a59b86d89baf
Simeon Lees <simeon.k.lees@gmail.com>
2023-03-07 10:09:56 +0000

    Add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from 'next/app';
 import { Inter } from '@next/font/google';
 import '../styles/index.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { GA_TRACKING_ID, pageview } from '../lib/gtag';
+import Head from 'next/head';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -8,9 +13,42 @@ const inter = Inter({
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <main className={`${inter.variable} font-sans`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <main className={`${inter.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
