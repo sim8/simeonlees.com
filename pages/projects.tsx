ed28b0a4b13a41ae83bc048a27debe17d90d2306
Simeon Lees <simeon.k.lees@gmail.com>
2023-05-19 09:39:58 +0100

    Mobile-first projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -21,18 +21,23 @@ export default function Projects() {
             <ul className="unstyled">
               {projects.map(
                 ({ slug, title, description, previewUrl, link }) => (
-                  <li key={slug} className="flex items-start mb-10">
-                    <div className="flex-1 mr-10">
+                  <li
+                    key={slug}
+                    className="flex flex-col sm:flex-row items-start mb-10"
+                  >
+                    <div className="flex-1 sm:mr-10">
                       <h2 className="text-2xl mb-4 leading-snug">{title}</h2>
                       <p className="mb-5">{description}</p>
                       <ButtonLink href={link}>Go</ButtonLink>
                     </div>
-                    <Image
-                      src={previewUrl}
-                      alt={title}
-                      width={248}
-                      height={192}
-                    />
+                    <div className="mt-3 sm:mt-1 bg-black aspect-[2/1] sm:aspect-[4/3] w-full sm:w-48 relative">
+                      <Image
+                        src={previewUrl}
+                        alt={title}
+                        fill={true}
+                        className="object-cover"
+                      />
+                    </div>
                   </li>
                 )
               )}
